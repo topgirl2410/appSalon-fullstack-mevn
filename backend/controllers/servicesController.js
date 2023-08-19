35bf810901f460e30e72dc3f8876748dba6eae65
topgirl2410 <maria.topgirl82@gmail.com>
2023-08-19 16:51:38 +0200

    agregando controlador de servicios

---
+++ backend/controllers/servicesController.js
@@ -0,0 +1,10 @@
+import { services } from '../data/beautyServices.js'
+
+const getServices = (req, res) => {
+
+    res.json(services)
+}
+
+export {
+    getServices
+}
--- backend/routes/servicesRoutes.js
+++ backend/routes/servicesRoutes.js
@@ -1,12 +1,10 @@
 import express from 'express'
-import { services } from '../data/beautyServices.js'
+
+import { getServices } from '../controllers/servicesController.js'
 
 const router = express.Router()
 
 
-router.get('/', (req, res) => {
+router.get('/', getServices)
 
-    res.json(services)
-})
-
-export default router
+export default router;
