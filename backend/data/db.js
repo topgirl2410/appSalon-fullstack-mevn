8bb4c75752771f0f8d64363f47cc6f283c23722b
topgirl2410 <maria.topgirl82@gmail.com>
2023-08-19 15:49:17 +0200

    db conectada correctamente

--- backend/data/db.js
+++ backend/data/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 export const db = async () => {
     try {
-
+        const db = await mongoose.connect('mongodb+srv://princess82:password82@cluster0.3daziy1.mongodb.net/?retryWrites=true&w=majority')
+        console.log('Bd conectada correctamente')
     } catch (error) {
         console.log(`Error: ${error.message}`);
         process.exit(1)
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,23 @@
 import express from 'express';
 import servicesRoutes from './routes/servicesRoutes.js'
 
+import { db } from './data/db.js';
+
+// Configurar App
 const app = express();
 
+// Conectar Db
+db();
+
+// Definir una ruta
 app.use('/api/services', servicesRoutes)
 
+
+// Definir puerto
 const PORT = process.env.PORT || 4000;
 
+
+// Arrancar la App
 app.listen(PORT, () => {
     console.log('El servidor se esta ejecutando correctamente en el puerto:', PORT)
 })
