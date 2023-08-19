1ae2b1db0cf7e8a19011b783fc47a2a43900ede5
topgirl2410 <maria.topgirl82@gmail.com>
2023-08-20 00:00:11 +0200

    eliminando multiples registros

--- backend/data/seed.js
+++ backend/data/seed.js
@@ -1,9 +1,33 @@
-function seedDB() {
-    console.log('desde seedDB')
+import dotenv from 'dotenv'
+import colors from 'colors'
+import { db } from '../config/db.js'
+import Services from '../models/Services.js'
+import { services } from './beautyServices.js'
+
+
+dotenv.config()
+await db()
+
+async function seedDB() {
+    try {
+        await Services.insertMany(services)
+        console.log('Se agregaron los datos correctamente'.rainbow.bold);
+        process.exit()
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
 }
 
-function clearDB() {
-    console.log('desde clearDB')
+async function clearDB() {
+    try {
+        await Services.deleteMany()
+        console.log('Los datos se borraron correctamente'.cyan.bold);
+        process.exit()
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
 }
 
 if (process.argv[2] === '--import') {
