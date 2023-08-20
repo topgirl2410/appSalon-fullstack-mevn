df8d8abed3670dea7f6ada40a68253a359799655
topgirl2410 <maria.topgirl82@gmail.com>
2023-08-20 23:23:47 +0200

    comenzando el layout de citas

--- frontend/src/views/appointments/AppointmentsLayout.vue
+++ frontend/src/views/appointments/AppointmentsLayout.vue
@@ -4,9 +4,28 @@
 
 <template>
     <div>
-        <div>
-            <h1>Appoinment Layout</h1>
+        <div class="flex justify-between">
+            <h1 class="text-2xl lg:text-6xl font-black text-white">Extreme Beauty</h1>
+            <div class="flex flex-col space-y-5">
+                <div class="flex gap-2 items-center">
+                    <p class="text-white text-right">Usuario: María</p>
+                    <button type="button"
+                        class="bg-pink-600 hover:bg-pink-700 p-2 text-white uppercase text-xs font-extrabold rounded-lg">Cerrar
+                        Sesión</button>
+                </div>
+
+                <nav class="flex gap-2 items-center justify-end">
+                    <button class="p-3 text-gray-200 uppercase text-xs font-black rounded-lg">Mis
+                        Citas</button>
+
+                    <RouterLink to="{name: 'new-appoinment'}"
+                        class="p-3 text-gray-200 uppercase text-xs font-black rounded-lg bg-blue-700 hover:bg-blue-800">
+                        Nueva Cita
+                    </RouterLink>
+                </nav>
+            </div>
         </div>
+
         <main>
             <RouterView />
         </main>
